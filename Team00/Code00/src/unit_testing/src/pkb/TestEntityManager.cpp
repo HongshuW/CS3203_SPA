e67601d4be55b45605b0778de2b34ff76ba8ffce
HongshuW <hongshu.wang.uni@gmail.com>
2022-09-01 01:36:26 +0800

    Add TestEntityManager unit test

---
+++ Team00/Code00/src/unit_testing/src/pkb/TestEntityManager.cpp
@@ -0,0 +1,15 @@
+//
+// Created by hongshu wang on 1/9/22.
+//
+
+#include "catch.hpp"
+#include "pkb/EntityManager.h"
+
+using namespace std;
+
+TEST_CASE("Test Entity Manager") {
+    SECTION("Get Variables in a Table") {
+        Table<string> * tablePtr = EntityManager::getVariables();
+        REQUIRE(tablePtr == &EntityManager::variableTable);
+    }
+}
