6d00cd56fe62234d7c05282790668f590bef82d5
HongshuW <hongshu.wang.uni@gmail.com>
2022-08-31 09:33:03 +0800

    PKB: Add interfaces for PS and QPS

---
+++ Team00/Code00/src/spa/src/pkb/DataRetriever.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hongshu wang on 31/8/22.
+//
+
+#include "DataRetriever.h"
---
+++ Team00/Code00/src/spa/src/pkb/DataRetriever.h
@@ -0,0 +1,8 @@
+//
+// Created by hongshu wang on 31/8/22.
+//
+
+#ifndef SPA_DATARETRIEVER_H
+#define SPA_DATARETRIEVER_H
+
+#endif //SPA_DATARETRIEVER_H
