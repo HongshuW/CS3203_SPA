c2d9098399eef9555a8f107e3426e154f0895a45
tanweien <tanweien8382@gmail.com>
2022-09-03 17:18:18 +0800

    Add source parser and test

--- Team00/Code00/src/spa/src/parser/Parser.h
+++ Team00/Code00/src/spa/src/parser/Parser.h
@@ -5,19 +5,35 @@
 #include <vector>
 #include "ast/ProcedureNode.h"
 #include "ast/AssignNode.h"
+#include "ast/VariableNode.h"
 #include "ast/ASTNode.h"
+#include "ast/ProgramNode.h"
 
 #ifndef SPA_PARSER_H
 #define SPA_PARSER_H
 
 #endif //SPA_PARSER_H
 
+using namespace std;
+
 namespace SourceParser {
     class Parser {
     private:
-        std::vector<std::string> tokens;
-        std::shared_ptr<ProcedureNode> parseProcedure
+        vector<std::string> tokens;
+        unsigned int currIdx;
+        string peek();
+        string previous();
+        string pop();
+        bool match(string s);
+        bool expect(string s);
+        shared_ptr<ProcedureNode> parseProcedure();
+        shared_ptr<VariableNode> parseVariable();
+//        shared_ptr<ExpressionNode> parseExpression();
+        shared_ptr<NumberNode> parseNumber();
+        shared_ptr<AssignNode> parseAssign();
+
     public:
-        ASTNode parse();
+        explicit Parser(std::vector<std::string> tokens);
+        shared_ptr<ProgramNode> parse();
     };
 }
--- Team00/Code00/src/unit_testing/src/parser/TestTokenizer.cpp
+++ Team00/Code00/src/unit_testing/src/parser/TestTokenizer.cpp
@@ -43,8 +43,8 @@ TEST_CASE ("Test Tokenizer") {
     }
 
     SECTION ("test simple source program") {
-        std::string source = "procedure {"
-                                "x = 1"
+        std::string source = "procedure testProcedure {"
+                                "x = 1;"
                              "}";
 
         Tokenizer tokenizer = Tokenizer(source);
@@ -54,7 +54,31 @@ TEST_CASE ("Test Tokenizer") {
 //            std::cout << tokens[i] << ' ';
 
         auto expected = std::vector<std::string>(
-                {"procedure", "{", "x", "=", "1", "}"});
+                {"procedure", "testProcedure", "{", "x", "=", "1", ";", "}"});
+
+        REQUIRE(std::equal(begin(tokens), end(tokens), begin(expected),
+                           end(expected),
+                           [](const std::string l, const std::string o) {
+                               return l.compare(o) == 0;
+                           }));
+    }
+
+    SECTION ("test source program with multiple procedures") {
+        std::string source = "procedure firstProcedure {"
+                             "x = 1;"
+                             "}"
+                             "procedure secondProcedure {"
+                             "y = 2;"
+                             "}";
+
+        Tokenizer tokenizer = Tokenizer(source);
+        auto tokens = tokenizer.tokenize();
+
+//        for(int i = 0; i < tokens.size(); i++)
+//            std::cout << tokens[i] << ' ';
+
+        auto expected = std::vector<std::string>(
+                {"procedure", "firstProcedure", "{", "x", "=", "1", ";", "}", "procedure", "secondProcedure", "{", "y", "=", "2", ";", "}"});
 
         REQUIRE(std::equal(begin(tokens), end(tokens), begin(expected),
                            end(expected),
