75754eb15ef759b69e3afc746e725f97c5fe05c3
aaron-ljx <70706659+aaron-ljx@users.noreply.github.com>
2022-09-09 00:24:33 +0800

    fixed error

--- Team41/Code41/src/spa/src/AST/ProgramNode.cpp
+++ Team41/Code41/src/spa/src/AST/ProgramNode.cpp
@@ -5,5 +5,5 @@
 #include "ProgramNode.h"
 
 ProgramNode::ProgramNode(std::vector<std::shared_ptr<ProcedureNode>> procedureList) : procedureList(procedureList) {
-    this->setNodeType("program")
+    this->setNodeType("program");
 }
