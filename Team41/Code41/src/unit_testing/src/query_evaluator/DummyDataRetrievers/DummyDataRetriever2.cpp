ccc23b36edf7f79584a448de6674ac11d8d81ee6
Ruofan <ruofan.wang@u.nus.edu>
2022-09-08 20:07:09 +0800

    add test case skeleton

---
+++ Team41/Code41/src/unit_testing/src/query_evaluator/DummyDataRetrievers/DummyDataRetriever2.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nafour on 8/9/22.
+//
+
+#include "DummyDataRetriever2.h"
---
+++ Team41/Code41/src/unit_testing/src/query_evaluator/DummyDataRetrievers/DummyDataRetriever2.h
@@ -0,0 +1,27 @@
+//
+// Created by Nafour on 8/9/22.
+//
+
+#ifndef SPA_DUMMYDATARETRIEVER2_H
+#define SPA_DUMMYDATARETRIEVER2_H
+/*
+ * procedure p2 {
+ *    read x;
+ *    read y;
+ *    while ( y > 0 ) {
+ *          x = x + 1
+ *          if ( x > 2 ) {
+ *              x + 2
+ *          }
+ *         y = y - 1
+ *    }
+ *    print x
+ * }
+ */
+
+class DummyDataRetriever2 {
+
+};
+
+
+#endif //SPA_DUMMYDATARETRIEVER2_H
