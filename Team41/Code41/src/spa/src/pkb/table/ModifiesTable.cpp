f102e69a858c05c9875802397ff5d66281e48b98
HongshuW <hongshu.wang.uni@gmail.com>
2022-09-08 20:32:17 +0800

    Table: add classes for Relation tables

---
+++ Team41/Code41/src/spa/src/pkb/table/ModifiesTable.cpp
@@ -0,0 +1,9 @@
+//
+// Created by hongshu wang on 8/9/22.
+//
+
+#include "ModifiesTable.h"
+
+ModifiesTable::ModifiesTable() {
+    header = vector<string>{"$modifier", "$modified"};
+}
---
+++ Team41/Code41/src/spa/src/pkb/table/RelationTable.h
@@ -0,0 +1,14 @@
+//
+// Created by hongshu wang on 8/9/22.
+//
+
+#ifndef SPA_RELATIONTABLE_H
+#define SPA_RELATIONTABLE_H
+
+#include "Table.h"
+
+class RelationTable : public Table {
+};
+
+
+#endif //SPA_RELATIONTABLE_H
