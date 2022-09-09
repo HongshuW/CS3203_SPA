91e15ee6f54dd3a3a43fe817223d4b2b725d0de4
HongshuW <hongshu.wang.uni@gmail.com>
2022-09-09 20:22:41 +0800

    pkb: add and implement KeyNotFoundException

---
+++ Team41/Code41/src/spa/src/pkb/exception/KeyNotFoundException.cpp
@@ -0,0 +1,15 @@
+//
+// Created by hongshu wang on 9/9/22.
+//
+
+#include "KeyNotFoundException.h"
+
+KeyNotFoundException::KeyNotFoundException(string columnName, string key) {
+    message = "Key " + key + " is not found in column " + columnName + ".";
+}
+
+char * KeyNotFoundException::what() {
+    char output[message.size()];
+    strcpy(output, message.c_str());
+    return output;
+}
---
+++ Team41/Code41/src/spa/src/pkb/exception/KeyNotFoundException.h
@@ -0,0 +1,23 @@
+//
+// Created by hongshu wang on 9/9/22.
+//
+
+#ifndef SPA_KEYNOTFOUNDEXCEPTION_H
+#define SPA_KEYNOTFOUNDEXCEPTION_H
+
+#include <exception>
+#include <string>
+
+using namespace std;
+
+class KeyNotFoundException : public exception {
+public:
+    explicit KeyNotFoundException(string columnName, string key);
+    char * what();
+
+private:
+    string message;
+};
+
+
+#endif //SPA_KEYNOTFOUNDEXCEPTION_H
