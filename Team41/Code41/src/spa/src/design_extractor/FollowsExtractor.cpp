99f7065836ce6cf7532a5bdb7f80920ded8aafce
aaron-ljx <70706659+aaron-ljx@users.noreply.github.com>
2022-09-10 18:25:29 +0800

    changed traversal to bfs

--- Team41/Code41/src/spa/src/design_extractor/FollowsExtractor.cpp
+++ Team41/Code41/src/spa/src/design_extractor/FollowsExtractor.cpp
@@ -2,6 +2,7 @@
 // Created by Aaron on 10/9/22.
 //
 
+#include <queue>
 #include "FollowsExtractor.h"
 #include "AST/utils/ASTUtils.h"
 
@@ -13,35 +14,102 @@ void FollowsExtractor::extractFollows(shared_ptr<ProgramNode> programNode) {
     }
 }
 
-void FollowsExtractor::extractFollowsFromProcedureNode(shared_ptr<ProcedureNode> procedureNode) {
-    vector<shared_ptr<StmtNode>> stmtList = procedureNode->stmtList;
-    vector<shared_ptr<StmtNode>> followsList;
-    for (auto stmtNode : stmtList) {
-        followsList.push_back(stmtNode);
-        NodeType nodeType = ASTUtils::getNodeTypeByName(stmtNode);
-        switch (nodeType) {
-            case AST::IF_NODE: {
-                shared_ptr<IfNode> ifNode = dynamic_pointer_cast<IfNode>(stmtNode);
-                vector<shared_ptr<StmtNode>> ifStmtList = ifNode->ifStmtList;
-                vector<shared_ptr<StmtNode>> elseStmtList = ifNode->elseStmtList;
-                extractFollowsFromStmtList(ifStmtList);
-                extractFollowsFromStmtList(elseStmtList);
-                break;
-            }
+vector<vector<shared_ptr<StmtNode>>>
+        FollowsExtractor::extractFollowsFromProcedureNode(shared_ptr<ProcedureNode> procedureNode) {
+    vector<vector<shared_ptr<StmtNode>>> listOfStmtList;
+    queue<vector<shared_ptr<StmtNode>>> queue;
+    queue.push(procedureNode->stmtList);
+    while(!queue.empty()) {
+        auto stmtList = queue.front();
+        listOfStmtList.push_back(stmtList);
+        for(auto stmtNode: stmtList) {
+            NodeType nodeType = ASTUtils::getNodeTypeByName(stmtNode);
+            switch (nodeType) {
+                case AST::IF_NODE: {
+                    shared_ptr<IfNode> ifNode = dynamic_pointer_cast<IfNode>(stmtNode);
+                    vector<shared_ptr<StmtNode>> ifStmtList = ifNode->ifStmtList;
+                    vector<shared_ptr<StmtNode>> elseStmtList = ifNode->elseStmtList;
+                    queue.push(ifStmtList);
+                    queue.push(elseStmtList);
+                }
 
-            case AST::WHILE_NODE: {
-                shared_ptr<WhileNode> whileNode = dynamic_pointer_cast<WhileNode>(stmtNode);
-                vector<shared_ptr<StmtNode>> whileStmtList = whileNode->stmtList;
-                extractFollowsFromStmtList(whileStmtList);
+                case AST::WHILE_NODE: {
+                    shared_ptr<WhileNode> whileNode = dynamic_pointer_cast<WhileNode>(stmtNode);
+                    vector<shared_ptr<StmtNode>> whileStmtList = whileNode->stmtList;
+                    queue.push(whileStmtList);
+                }
+
+                default:
+                    break;
             }
-            
-            default:
-                break;
         }
     }
+
+    return listOfStmtList;
 }
 
-void FollowsExtractor::extractFollowsFromStmtList(vector<shared_ptr<StmtNode>> stmtList) {
 
-}
+
+//void FollowsExtractor::extractFollowsFromProcedureNode(shared_ptr<ProcedureNode> procedureNode) {
+//    vector<shared_ptr<StmtNode>> stmtList = procedureNode->stmtList;
+//    vector<shared_ptr<StmtNode>> followsList;
+//    for (auto stmtNode : stmtList) {
+//        followsList.push_back(stmtNode);
+//        NodeType nodeType = ASTUtils::getNodeTypeByName(stmtNode);
+//        switch (nodeType) {
+//            case AST::IF_NODE: {
+//                shared_ptr<IfNode> ifNode = dynamic_pointer_cast<IfNode>(stmtNode);
+//                vector<shared_ptr<StmtNode>> ifStmtList = ifNode->ifStmtList;
+//                vector<shared_ptr<StmtNode>> elseStmtList = ifNode->elseStmtList;
+//                extractFollowsFromStmtList(ifStmtList);
+//                extractFollowsFromStmtList(elseStmtList);
+//                break;
+//            }
+//
+//            case AST::WHILE_NODE: {
+//                shared_ptr<WhileNode> whileNode = dynamic_pointer_cast<WhileNode>(stmtNode);
+//                vector<shared_ptr<StmtNode>> whileStmtList = whileNode->stmtList;
+//                extractFollowsFromStmtList(whileStmtList);
+//            }
+//
+//            default:
+//                break;
+//        }
+//    }
+//    listOfFollowLists.push_back(followsList);
+//}
+//
+//void FollowsExtractor::extractFollowsFromStmtList(vector<shared_ptr<StmtNode>> stmtList) {
+//    vector<shared_ptr<StmtNode>> followsList;
+//    for (auto stmtNode : stmtList) {
+//        followsList.push_back(stmtNode);
+//        NodeType nodeType = ASTUtils::getNodeTypeByName(stmtNode);
+//        switch (nodeType) {
+//            case AST::IF_NODE: {
+//                shared_ptr<IfNode> ifNode = dynamic_pointer_cast<IfNode>(stmtNode);
+//                vector<shared_ptr<StmtNode>> ifStmtList = ifNode->ifStmtList;
+//                vector<shared_ptr<StmtNode>> elseStmtList = ifNode->elseStmtList;
+//                extractFollowsFromStmtList(ifStmtList);
+//                extractFollowsFromStmtList(elseStmtList);
+//                break;
+//            }
+//
+//            case AST::WHILE_NODE: {
+//                shared_ptr<WhileNode> whileNode = dynamic_pointer_cast<WhileNode>(stmtNode);
+//                vector<shared_ptr<StmtNode>> whileStmtList = whileNode->stmtList;
+//                extractFollowsFromStmtList(whileStmtList);
+//            }
+//
+//            default:
+//                break;
+//        }
+//    }
+//    listOfFollowLists.push_back(followsList);
+//}
+//
+//void FollowsExtractor::saveFollowsToPKB(vector<shared_ptr<StmtNode>> followsList) {
+//    for (auto stmtNode : followsList) {
+//        ASTUtils::getNodePtrToLineNumMap();
+//    }
+//}
 
