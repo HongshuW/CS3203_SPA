1ec0ec98aa538d4de3cd3da634fd5e62fa38574b
HongshuW <hongshu.wang.uni@gmail.com>
2022-09-12 11:41:11 +0800

    PKB: add PatternManager class

---
+++ Team41/Code41/src/spa/src/pkb/PatternManager.cpp
@@ -0,0 +1,8 @@
+//
+// Created by hongshu wang on 12/9/22.
+//
+
+#include "PatternManager.h"
+
+PatternTable PatternManager::patternTable = PatternTable();
+
---
+++ Team41/Code41/src/spa/src/pkb/PatternManager.h
@@ -0,0 +1,19 @@
+//
+// Created by hongshu wang on 12/9/22.
+//
+
+#ifndef SPA_PATTERNMANAGER_H
+#define SPA_PATTERNMANAGER_H
+
+#include "table/PatternTable.h"
+
+class PatternManager {
+public:
+    // getter
+    // setter
+private:
+    static PatternTable patternTable;
+};
+
+
+#endif //SPA_PATTERNMANAGER_H
