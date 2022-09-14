5c257d0f9ca722aaf34aeb4c7c4eb7158246595b
Linxcathyyy <linxingchen0428cathy@gmail.com>
2022-09-14 14:12:29 +0800

    Fix bugs for CondExprParser

--- Team41/Code41/src/unit_testing/src/parser/TestCondExprParser.cpp
+++ Team41/Code41/src/unit_testing/src/parser/TestCondExprParser.cpp
@@ -189,6 +189,14 @@ TEST_CASE("Test SP CondExprNode Parser") {
         REQUIRE_NOTHROW(parser.parse());
     }
 
+    SECTION("a == (b + c)") {
+        vector<string> tokens = vector<string>(
+                {"a", "==", "(", "b", "+", "c", ")"});
+        bool isConnected = false;
+        CondExprParser parser = CondExprParser(tokens, isConnected);
+        REQUIRE_NOTHROW(parser.parse());
+    }
+
     //! Test for invalid inputs
     SECTION("(x == y && (z > 2)") {
         vector<string> tokens = vector<string>(
