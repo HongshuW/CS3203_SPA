643110ce8a700b90b425e4725f8c0aeabfd953fc
Linxcathyyy <linxingchen0428cathy@gmail.com>
2022-09-15 18:29:20 +0800

    Add SPAManager skeleton class

---
+++ Team41/Code41/src/spa/src/spa_manager/SPAManager.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Xingchen Lin on 15/9/22.
+//
+
+#include "SPAManager.h"
+
+void SPAManager::parse(string &filename) {
+
+}
+
+vector<string> SPAManager::evaluate(string& query) {
+
+}
