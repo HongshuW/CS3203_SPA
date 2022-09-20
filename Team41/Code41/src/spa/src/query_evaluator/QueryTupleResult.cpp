919e8fe85029540c4dee4bb8d30a97037eeffccd
Ruofan <ruofan.wang@u.nus.edu>
2022-09-20 15:23:20 +0800

    refactor query evaluator

---
+++ Team41/Code41/src/spa/src/query_evaluator/QueryTupleResult.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Nafour on 20/9/22.
+//
+
+#include "QueryTupleResult.h"
+
+namespace QE {
+    QueryTupleResult::QueryTupleResult(shared_ptr<vector<Elem>> returnTuple): returnTuple(returnTuple) {
+
+    }
+} // QE
