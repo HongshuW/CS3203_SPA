852d3384a62440f7d2dd55d9ab5d495f7a74f333
aaron-ljx <70706659+aaron-ljx@users.noreply.github.com>
2022-10-08 03:01:50 +0800

    added if pattern extractor

--- Team41/Code41/src/spa/src/design_extractor/PatternExtractor.cpp
+++ Team41/Code41/src/spa/src/design_extractor/PatternExtractor.cpp
@@ -4,6 +4,7 @@
 
 #include "PatternExtractor.h"
 #include "AST/utils/ASTUtils.h"
+#include <queue>
 namespace DE {
 
     vector<pair<pair<int, string>, std::shared_ptr<AssignNode>>>
@@ -17,7 +18,8 @@ namespace DE {
     }
 
     void PatternExtractor::extractPatternHelper(shared_ptr<ASTNode> node,
-                                                shared_ptr<vector<pair<pair<int, string>, std::shared_ptr<AssignNode>>>> resultVec, shared_ptr<unordered_map<shared_ptr<StmtNode>, int>> stmtNumbers) {
+                                                shared_ptr<vector<pair<pair<int, string>, std::shared_ptr<AssignNode>>>> resultVec,
+                                                shared_ptr<unordered_map<shared_ptr<StmtNode>, int>> stmtNumbers) {
         NodeType nodeType = ASTUtils::getNodeType(node);
 
         if (nodeType == AST::PROCEDURE_NODE) {
@@ -53,5 +55,100 @@ namespace DE {
             }
         }
 
+    }
+
+    list<vector<string>> PatternExtractor::extractIfPattern(shared_ptr<AST::ProgramNode> programNode) {
+        list<vector<string>> output;
+        auto stmtNumbers = ASTUtils::getNodePtrToLineNumMap(programNode);
+        for (auto procedureNode: programNode -> procedureList) {
+            queue<vector<shared_ptr<StmtNode>>> queue;
+            queue.push(procedureNode->stmtList);
+            while(!queue.empty()) {
+                auto stmtList = queue.front();
+                queue.pop();
+                for (auto stmtNode: stmtList) {
+                    NodeType nodeType = ASTUtils::getNodeType(stmtNode);
+                    switch (nodeType) {
+                        case AST::IF_NODE: {
+                            shared_ptr<IfNode> ifNode = dynamic_pointer_cast<IfNode>(stmtNode);
+                            int stmtNo = stmtNumbers->at(ifNode);
+                            vector<string> varList = condExprNodeHandler(ifNode->condExpr);
+                            for (string var: varList) {
+                                vector<string> ifEntry;
+                                ifEntry.push_back(to_string(stmtNo));
+                                ifEntry.push_back(var);
+                                output.push_back(ifEntry);
+                            }
+
+                            vector<shared_ptr<StmtNode>> ifStmtList = ifNode->ifStmtList;
+                            vector<shared_ptr<StmtNode>> elseStmtList = ifNode->elseStmtList;
+                            queue.push(ifStmtList);
+                            queue.push(elseStmtList);
+                            break;
+                        }
+                        case AST::WHILE_NODE: {
+                            shared_ptr<WhileNode> whileNode = dynamic_pointer_cast<WhileNode>(stmtNode);
+                            vector<shared_ptr<StmtNode>> whileStmtList = whileNode->stmtList;
+                            queue.push(whileStmtList);
+                            break;
+                        }
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        return output;
+    }
+
+    vector<string> PatternExtractor::condExprNodeHandler(shared_ptr<CondExprNode> condExpr) {
+        vector<shared_ptr<RelExprNode>> relExprNodeList;
+        vector<string> varList;
+        PatternExtractor::getRelExprNodesDFS(condExpr, relExprNodeList);
+        for (auto relExpr: relExprNodeList) {
+            relExprNodeHandler(relExpr, varList);
+        }
+        return varList;
+    }
+
+    void PatternExtractor::relExprNodeHandler(shared_ptr<RelExprNode> relExpr,
+                                                        vector<string>& varList) {
+        auto exprLHS = relExpr->exprNodeLHS;
+        auto exprRHS = relExpr->exprNodeRHS;
+        exprNodeHandler(exprLHS, varList);
+        exprNodeHandler(exprRHS, varList);
+    }
+
+    void PatternExtractor::exprNodeHandler(shared_ptr<ExprNode> expr, vector<string>& varList) {
+        getVarFromExprNodesDFS(expr, varList);
+    }
+
+    void PatternExtractor::getVarFromExprNodesDFS(shared_ptr<ExprNode> expr, vector<string>& varList) {
+        if (expr->isVariableNode()) {
+            varList.push_back(expr->expr);
+        }
+
+        if (expr->isOperatorNode()) {
+            auto LHS = expr->left;
+            auto RHS = expr->right;
+            getVarFromExprNodesDFS(LHS, varList);
+            getVarFromExprNodesDFS(RHS, varList);
+        }
+    }
+
+    void PatternExtractor::getRelExprNodesDFS(shared_ptr<CondExprNode> condExpr,
+                                              vector<shared_ptr<RelExprNode>> &relExprNodeList) {
+        if(condExpr->relExprNode != nullptr) {
+            relExprNodeList.push_back(condExpr->relExprNode);
+        }
+
+        if (condExpr->condExprLHS != nullptr) {
+            getRelExprNodesDFS(condExpr->condExprLHS, relExprNodeList);
+        }
+
+        if (condExpr->condExprRHS != nullptr) {
+            getRelExprNodesDFS(condExpr->condExprLHS, relExprNodeList);
+        }
     }
 } // DE
--- Team41/Code41/src/spa/src/design_extractor/PatternExtractor.h
+++ Team41/Code41/src/spa/src/design_extractor/PatternExtractor.h
@@ -7,8 +7,10 @@
 
 #include <string>
 #include <vector>
+#include <list>
 #include "AST/AssignNode.h"
 #include "AST/ProgramNode.h"
+#include "AST/CondExprNode.h"
 #include <unordered_map>
 namespace DE {
 
@@ -16,6 +18,14 @@ namespace DE {
         static void extractPatternHelper(shared_ptr<ASTNode> node, shared_ptr<vector<pair<pair<int, string>, std::shared_ptr<AssignNode>>>> resultVec, shared_ptr<unordered_map<shared_ptr<StmtNode>, int>> stmtNumbers);
     public:
         static vector<pair<pair<int, string>, std::shared_ptr<AssignNode>>> extractPattern(shared_ptr<ProgramNode> programNode);
+        static list<vector<string>> extractIfPattern(shared_ptr<ProgramNode> programNode);
+
+        static vector<string> condExprNodeHandler(shared_ptr<CondExprNode> condExpr);
+        static void relExprNodeHandler(shared_ptr<RelExprNode> relExpr, vector<string>& varList);
+        static void exprNodeHandler(shared_ptr<ExprNode> expr, vector<string>& varList);
+        static void getRelExprNodesDFS(shared_ptr<CondExprNode> condExpr,
+                                vector<shared_ptr<RelExprNode>>& relExprNodeList);
+        static void getVarFromExprNodesDFS(shared_ptr<ExprNode> expr, vector<string>& varList);
     };
 
 } // DE
