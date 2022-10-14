939df3d0b2c99aa29a1090307ca07cc65a85e0e0
tanweien <tanweien8382@gmail.com>
2022-10-15 01:08:07 +0800

    add test for affects

--- Team41/Tests41/milestone-2/run-milestone-2.sh
+++ Team41/Tests41/milestone-2/run-milestone-2.sh
@@ -1,10 +1,9 @@
 # running milestone 2 tests
-../../Code41/cmake-build-debug-coverage/src/autotester/autotester affects/simple_source.txt affects/simple_one_affects_clause_queries.txt milestone-2-out/affects-analysis.xml
-../../Code41/cmake-build-debug-coverage/src/autotester/autotester basic-spa/deep_nesting_source.txt basic-spa/deep_nesting_multiple_clauses_queries.txt milestone-2-out/basic-spa-analysis.xml
-../../Code41/cmake-build-debug-coverage/src/autotester/autotester calls/multiple_procedures_source.txt basic-spa/multiple_procedures_one_calls_clause_queries.txt milestone-2-out/calls-analysis.xml
-../../Code41/cmake-build-debug-coverage/src/autotester/autotester multi-clause/deep_nesting_source.txt multi-clause/deep_nesting_multiple_clauses_queries.txt milestone-2-out/multiple-clauses-analysis.xml
-../../Code41/cmake-build-debug-coverage/src/autotester/autotester next/deep_nesting_source.txt next/deep_nesting_one_next_clause_queries.txt milestone-2-out/next-analysis.xml
-../../Code41/cmake-build-debug-coverage/src/autotester/autotester pattern/container_patterns_source.txt pattern/container_patterns_one_pattern_clause_queries.txt milestone-2-out/pattern-analysis.xml
-../../Code41/cmake-build-debug-coverage/src/autotester/autotester with/deep_nesting_source.txt with/deep_nesting_one_with_clause_queries.txt milestone-2-out/with-analysis.xml
-
-
+../../Code41/cmake-build-debug/src/autotester/autotester affects/simple_source.txt affects/simple_one_affects_clause_queries.txt milestone-2-out/affects-analysis.xml
+../../Code41/cmake-build-debug/src/autotester/autotester affects/simple_source_2.txt affects/simple_2_one_affects_clause_queries.txt milestone-2-out/affects-2-analysis.xml
+../../Code41/cmake-build-debug/src/autotester/autotester basic-spa/deep_nesting_source.txt basic-spa/deep_nesting_multiple_clauses_queries.txt milestone-2-out/basic-spa-analysis.xml
+../../Code41/cmake-build-debug/src/autotester/autotester calls/multiple_procedures_source.txt basic-spa/multiple_procedures_one_calls_clause_queries.txt milestone-2-out/calls-analysis.xml
+../../Code41/cmake-build-debug/src/autotester/autotester multi-clause/deep_nesting_source.txt multi-clause/deep_nesting_multiple_clauses_queries.txt milestone-2-out/multiple-clauses-analysis.xml
+../../Code41/cmake-build-debug/src/autotester/autotester next/deep_nesting_source.txt next/deep_nesting_one_next_clause_queries.txt milestone-2-out/next-analysis.xml
+../../Code41/cmake-build-debug/src/autotester/autotester pattern/container_patterns_source.txt pattern/container_patterns_one_pattern_clause_queries.txt milestone-2-out/pattern-analysis.xml
+../../Code41/cmake-build-debug/src/autotester/autotester with/deep_nesting_source.txt with/deep_nesting_one_with_clause_queries.txt milestone-2-out/with-analysis.xml
