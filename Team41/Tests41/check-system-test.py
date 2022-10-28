a59230f23f86ae4bf1474ab7441a74c83201abac
aaron-ljx <70706659+aaron-ljx@users.noreply.github.com>
2022-10-28 12:20:37 +0800

    update python file

--- Team41/Tests41/check-system-test.py
+++ Team41/Tests41/check-system-test.py
@@ -7,7 +7,7 @@ path_arg = sys.argv[1]
 print(path_arg)
 m2path = path_arg + '/Team41/Tests41/milestone-2/milestone-2-out'
 m1path = path_arg + '/Team41/Tests41/milestone-1/m1-out'
-if (os.path.exists(m2path) and os.path.exists(m1path)):
+if (os.path.exists(m2path)):
     m2AllPass = True
     m1AllPass = True
    
@@ -22,25 +22,23 @@ if (os.path.exists(m2path) and os.path.exists(m1path)):
                     if q_tag.tag == "failed":
                         m2AllPass = False
 
-    mydirm1 = Path(m1path)
-    for file in mydirm1.glob('*.xml'):
-    # parse xml file
-        m1tree = ET.parse(file)
-        root = m1tree.getroot()
-        for queries in root.findall("queries"):
-            for query in queries:
-                for q_tag in query:
-                    if q_tag.tag == "failed":
-                        m1AllPass = False
+    # mydirm1 = Path(m1path)
+    # for file in mydirm1.glob('*.xml'):
+    # # parse xml file
+    #     m1tree = ET.parse(file)
+    #     root = m1tree.getroot()
+    #     for queries in root.findall("queries"):
+    #         for query in queries:
+    #             for q_tag in query:
+    #                 if q_tag.tag == "failed":
+    #                     m1AllPass = False
 
-
-    if m2AllPass == False or m1AllPass == False:
+    if m2AllPass == False:
         print("fail")
     else:
         print("pass")
 else:
     print("directory does not exist")
-    print(m1path)
     print(m2path)
                 
     
