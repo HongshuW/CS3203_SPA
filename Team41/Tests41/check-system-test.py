5edf51a8b4fca1988a91a845fbbf2617ebce1e49
aaron-ljx <70706659+aaron-ljx@users.noreply.github.com>
2022-11-04 13:34:38 +0800

    update py and xml file

--- Team41/Tests41/check-system-test.py
+++ Team41/Tests41/check-system-test.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import xml.etree.ElementTree as ET
 
 path_arg = sys.argv[1]
+samplepath =  path_arg + '/Team41/Code41/tests'
 m2path = path_arg + '/Team41/Tests41/milestone-2/milestone-2-out'
 m1path = path_arg + '/Team41/Tests41/milestone-1/system-test-out'
 m3path = path_arg + '/Team41/Tests41/milestone-3/milestone-3-out'
@@ -12,10 +13,11 @@ env_file = os.getenv('GITHUB_ENV')
 
 failed_files = []
 
-if (os.path.exists(m2path) and os.path.exists(m1path) and os.path.exists(m3path)):
+if (os.path.exists(samplepath) and os.path.exists(m2path) and os.path.exists(m1path) and os.path.exists(m3path)):
     m2AllPass = True
     m1AllPass = True
     m3AllPass = True
+    sampleAllPass = True
    
     mydirm2 = Path(m2path)
     for file in mydirm2.glob('*.xml'):
@@ -55,8 +57,21 @@ if (os.path.exists(m2path) and os.path.exists(m1path) and os.path.exists(m3path)
                         m3 = "milestone-3-out/"
                         failed_files.append(m3 + os.path.basename(file))
                         m3AllPass = False
+                        
+    mydirsample = Path(samplepath)
+    for file in mydirsample.glob('*.xml'):
+    # parse xml file
+        sampletree = ET.parse(file)
+        root = sampletree.getroot()
+        for queries in root.findall("queries"):
+            for query in queries:
+                for q_tag in query:
+                    if q_tag.tag == "failed" or q_tag.tag == "timeout" or q_tag.tag == "exception" or q_tag.tag == "crash":
+                        sample = "sample-out/"
+                        failed_files.append(sample + os.path.basename(file))
+                        sampleAllPass = False
 
-    if m2AllPass == False or m1AllPass == False or m3AllPass == False:
+    if m2AllPass == False or m1AllPass == False or m3AllPass == False or sampleAllPass == False:
         failed_files = set(failed_files)
         fail_msg = "fail_message=The system tests that failed are: "
         for f in failed_files:
